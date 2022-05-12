4892f0f4337bbbf484a9e52d031b5bb2eb92c319
Lucas ARRIZABALAGA <arrizabalucas@gmail.com>
2022-05-12 16:11:11 +0200

    Creating BeachCard and begining of findYourOcean

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.scss";
+import FindYourOcean from "./components/FindYourOcean";
 import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <div>
         <LandingPage />
       </div>
