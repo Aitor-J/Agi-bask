551a1131faab544ffe2008da15b05905c22e9102
Aitor Jurquet <aitor.jurquet@gmail.com>
2022-05-12 20:59:26 +0200

    removing footer import

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.scss";
 import FindYourOcean from "./components/FindYourOcean";
 import LandingPage from "./components/LandingPage";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className='app'>
       <div>
         <LandingPage />
-        <Footer />
       </div>
     </div>
   );
