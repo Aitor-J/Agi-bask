b5165df242595a82603cd6e00b9fae110a93e6e2
Aitor Jurquet <aitor.jurquet@gmail.com>
2022-05-13 06:28:10 +0200

    Change color button of var

--- src/components/FindYourOcean.jsx
+++ src/components/FindYourOcean.jsx
@@ -11,14 +11,10 @@ const FindYourOcean = () => {
   return (
     <div className='findYourOcean'>
       <div className='findYourOcean__bg' />
-      <h1 className='findYourOcean__title'>
-        Find the perfect way to clean your ocean
-      </h1>
+      <h1 className='findYourOcean__title'>Find the perfect way to clean your ocean</h1>
       <div className='findYourOcean__selector'>
         <div className='findYourOcean__selector__city'>
-          <label className='findYourOcean__selector__city__title'>
-            Filter par ville
-          </label>
+          <label className='findYourOcean__selector__city__title'>Filter par ville</label>
 
           {cityNames.map((city, index) => (
             <button
