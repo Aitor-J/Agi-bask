e7dcd23a5d182a4c385b246d5ae6c772a327f45f
Lucas ARRIZABALAGA <arrizabalucas@gmail.com>
2022-05-13 06:41:54 +0200

    resolve nav position

--- src/components/FindYourForest.jsx
+++ src/components/FindYourForest.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import dataForest from "../data/dataforest";
+import NavBar from "./NavBar";
+
 import BeachCard from "./BeachCardList";
 import ForestCardList from "./ForestCardList";
 
@@ -13,22 +15,24 @@ const FindYourForest = () => {
   );
 
   return (
-    <div className='findYourForest'>
-      <div className='findYourForest__bg'>
-        <h1 className='findYourForest__bg__title'>
+    <div className="findYourForest">
+      <div className="findYourForest__bg">
+        <NavBar />
+        <h1 className="findYourForest__bg__title">
           Find the perfect way to clean your Forest
         </h1>
-        <div className='findYourForest__bg__selector'>
-          <div className='findYourForest__bg__selector__city'>
-            <label className='findYourForest__bg__selector__city__title'>
+        <div className="findYourForest__bg__selector">
+          <div className="findYourForest__bg__selector__city">
+            <label className="findYourForest__bg__selector__city__title">
               Filter par ville
             </label>
 
             {cityNames.map((city, index) => (
               <div
-                className='findYourForest__bg__selector__city__button'
+                className="findYourForest__bg__selector__city__button"
                 key={index}
-                value={city}>
+                value={city}
+              >
                 {city}
               </div>
             ))}
--- src/components/FindYourOcean.jsx
+++ src/components/FindYourOcean.jsx
@@ -1,31 +1,41 @@
 import React, { useState } from "react";
 import dataBeach from "../data/dataBeach";
 import BeachCardList from "./BeachCardList";
+import NavBar from "./NavBar";
 
 const FindYourOcean = () => {
   const [locations, setLocations] = useState([]);
 
   let cityNames = [];
-  dataBeach.map((beach) => !cityNames.includes(beach.location) && cityNames.push(beach.location));
+  dataBeach.map(
+    (beach) =>
+      !cityNames.includes(beach.location) && cityNames.push(beach.location)
+  );
 
   return (
-    <div className='findYourOcean'>
-      <div className='findYourOcean__bg' />
-      <h1 className='findYourOcean__title'>
+    <div className="findYourOcean">
+      <div className="findYourOcean__bg" />
+      <NavBar />
+      <h1 className="findYourOcean__title">
         Find the perfect way to clean your ocean
       </h1>
-      <div className='findYourOcean__selector'>
-        <div className='findYourOcean__selector__city'>
-          <label className='findYourOcean__selector__city__title'>
+      <div className="findYourOcean__selector">
+        <div className="findYourOcean__selector__city">
+          <label className="findYourOcean__selector__city__title">
             Filter par ville
           </label>
 
           {cityNames.map((city, index) => (
             <button
-              className={city === locations ? "findYourOcean__selector__city__buttonOn" : "findYourOcean__selector__city__button"}
+              className={
+                city === locations
+                  ? "findYourOcean__selector__city__buttonOn"
+                  : "findYourOcean__selector__city__button"
+              }
               key={index}
               value={city}
-              onClick={(e) => setLocations(e.target.value)}>
+              onClick={(e) => setLocations(e.target.value)}
+            >
               {city}
             </button>
           ))}
@@ -35,7 +45,9 @@ const FindYourOcean = () => {
         ? dataBeach
             .filter((beach) => beach.location == locations)
             .map((beach, index) => <BeachCardList beach={beach} key={index} />)
-        : dataBeach.map((beach, index) => <BeachCardList beach={beach} key={index} />)}
+        : dataBeach.map((beach, index) => (
+            <BeachCardList beach={beach} key={index} />
+          ))}
     </div>
   );
 };
