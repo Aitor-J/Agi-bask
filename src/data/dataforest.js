d37f811f1bfef2198ab6728be3ea764c1a329cf8
Astrid-Froitier <asfroitier@gmail.com>
2022-05-13 10:12:30 +0200

    update mission dataforest

--- src/data/dataforest.js
+++ src/data/dataforest.js
@@ -6,6 +6,8 @@ const databeach = [
     link: "",
     desc: "Forêt de pins, sentiers et promenades à vélo.",
     img: "../public/assets/forest-img/chiberta.jpg",
+    missions:
+      "Ramassage des déchets et des préventions qu'on les feux de forêts",
   },
   {
     id: 2,
@@ -14,6 +16,8 @@ const databeach = [
     link: "",
     desc: "Forêt de pins, sentiers et promenades à vélo.",
     img: "../public/assets/forest-img/pignada.jpg",
+    missions:
+      "Promenage en forêt avec guide pour vous présenter les différentes plantes",
   },
   {
     id: 3,
@@ -22,6 +26,8 @@ const databeach = [
     link: "",
     desc: "Vrai havre de paix, sentiers et promenades à vélo.",
     img: "../public/assets/forest-img/lazaret.jpg",
+    missions:
+      "Sensibilisation à l'impact humain sur l'environnement au travers de différents jeux",
   },
 
   {
@@ -31,6 +37,8 @@ const databeach = [
     link: "",
     desc: "Forêt au coeur du pays Basque, pour balades et randonnées",
     img: "../public/assets/forest-img/hasparren.jpg",
+    missions:
+      "Ramassage des déchets et sensibilisation au réchauffement climatique",
   },
   {
     id: 5,
@@ -39,6 +47,8 @@ const databeach = [
     link: "",
     desc: "Terrain varié, panorama très large sur les Pyrénées, quelques belles demeures.",
     img: "../public/assets/forest-img/ustaritz.jpg",
+    missions:
+      "Balade en forêt avec jeux pour sensibiliser sur les biens-faits du sport",
   },
 
   {
@@ -48,6 +58,7 @@ const databeach = [
     link: "",
     desc: "Forêt aux essences végétales variées, parfaite pour balade en famille.",
     img: "../public/assets/forest-img/urt.jpg",
+    missions: "Ramassage des déchets.",
   },
   {
     id: 7,
@@ -56,6 +67,8 @@ const databeach = [
     link: "",
     desc: "Bois très dense aux chemins sinueux",
     img: "../public/assets/forest-img/fagosse.jpg",
+    missions:
+      "Balade en forêt avec jeux pour sensibiliser sur les biens-faits du sport",
   },
   {
     id: 8,
@@ -64,6 +77,8 @@ const databeach = [
     link: "",
     desc: "Bois aux sentiers terreux, très humide avec beaucoup de champignons",
     img: "../public/assets/beach-img/saintpee.jpg",
+    missions:
+      "Balade en forêt avec jeux pour sensibiliser sur les biens-faits du sport",
   },
   {
     id: 9,
@@ -72,6 +87,7 @@ const databeach = [
     link: "",
     desc: "Forêt aux espèces diverses, parfaite pour des balades en vélo",
     img: "../public/assets/beach-img/lapin.jpg",
+    missions: "Balade dans un parc à la découverte des lapins",
   },
   {
     id: 10,
@@ -80,5 +96,7 @@ const databeach = [
     link: "",
     desc: "Plaine avec vue dégagé, pour de superbes panorama",
     img: "../public/assets/beach-img/ansot.jpg",
+    mission:
+      "Ramassage des déchets et sensibilisation au réchauffement climatique",
   },
 ];
